99e6dae083f220254ccf42e4f4cf96bb6811e506
ROK862 <88340948+ROK862@users.noreply.github.com>
2021-09-12 17:50:14 +0200

    Init

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ const ENDPOINT = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  */
  const fetchColors = async ({ name, hex, compName, compHex }) => { 
   const res = fetch(ENDPOINT);
+  /*
+   * Your code for filtering against datasets comes here.
+   * Please return a filtered json, or promise from fetch function.
+   * Run "npm test" to test your results before making your commit.
+   */
 
   return res;
 };
